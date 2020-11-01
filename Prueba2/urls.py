ccfcadd9e79b5d31bdb08ea2c09f92ec3467f48d
Edgar Villaseñor <edgar.andreus@gmail.com>
2020-11-01 00:19:10 -0300

    iniciando views

--- Prueba2/urls.py
+++ Prueba2/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
 
 
@@ -29,5 +29,6 @@ def contact(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
-    path('about/', contact)
+    path('about/', contact),
+    path('productos/', include('tienda.urls'))
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+
+def home (request):
+    productos = Producto.objects.all()
+    context = {'productos': productos}
+    return render(request, 'tienda/index.html', context)
