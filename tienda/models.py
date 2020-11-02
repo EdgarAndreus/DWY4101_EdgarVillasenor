5ef42de31fc34074278e0bcfd5b1bac453370caa
Edgar Villaseñor <edgar.andreus@gmail.com>
2020-11-02 03:39:38 -0300

    Creando Modelos

--- tienda/models.py
+++ tienda/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 
 # Create your models here.
@@ -6,7 +7,18 @@ from django.db import models
 class Producto(models.Model):
     nombre = models.CharField(max_length=200)
     codigo = models.CharField(max_length=200)
-    marca = models.CharField(max_length=100)
+    marca = models.ForeignKey('Marca', on_delete=models.SET_NULL, null=True)
     precio = models.IntegerField()
     descripcion = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Marca(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
 
